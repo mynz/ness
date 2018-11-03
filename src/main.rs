adbc6c07d00b37f3186e5d7b81fa18d8c1da7db9
mynz <rollingteapot@gmail.com>
2018-11-04 03:49:40 +0900

    read

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@
 #![warn(dead_code)]
 
 use std::fs::File;
-use std::io::{BufReader, Read};
+//use std::io::{BufReader, Read};
+use std::io::Read;
 
 fn load_bin(filename: String) {
     let fp = File::open(filename).unwrap();
-    let bin = std::io::BufReader::new(fp).bytes();
+    let mut reader = std::io::BufReader::new(fp);
 
+    let mut bin: Vec<u8> = Vec::with_capacity(4095);
+    let res = reader.read_to_end(&mut bin).unwrap();
+    println!("{:?}", res);
+
+    for (i, b) in bin.into_iter().enumerate() {
+        println!("b: {}P {:?}", i, b);
+    }
 }
 
 fn main() {
     println!("Hello, world!");
 
     load_bin("rom/sample1.nes".to_string());
-
 }
