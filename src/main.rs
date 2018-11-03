142027172d12a5c696a07d2e398b84803beee564
mynz <rollingteapot@gmail.com>
2018-11-03 23:49:28 +0900

    BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// main.rs
+
+#![warn(dead_code)]
+
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+fn load_bin(filename: String) {
+    let fp = File::open(filename).unwrap();
+    let bin = std::io::BufReader::new(fp).bytes();
+
+}
+
 fn main() {
     println!("Hello, world!");
+
+    load_bin("rom/sample1.nes".to_string());
+
 }
