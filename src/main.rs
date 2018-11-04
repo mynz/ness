62bd8838e97d58ffe83c4ee0f3be3545bdfbf74f
mynz <rollingteapot@gmail.com>
2018-11-05 01:43:14 +0900

    check signature

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,16 @@ fn load_bin(filename: String) {
     let fp = File::open(filename).unwrap();
     let mut reader = std::io::BufReader::new(fp);
 
-    let mut bin: Vec<u8> = Vec::with_capacity(4095);
+    let mut bin: Vec<u8> = Vec::with_capacity(1024 * 1024);
 
-    let mut header = [0u8; 16];
     let mut nread;
+    let mut header = [0u8; 16];
     nread = reader.read(&mut header).unwrap();
     println!("header: {}, {:?}", nread, header);
+
+    let sig = std::str::from_utf8(&header[0..4]).unwrap();
+    assert_eq!(sig, "NES\u{1a}");
+
 
     nread = reader.read_to_end(&mut bin).unwrap();
     println!("{:?}", nread);
