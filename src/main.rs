bee50cb202f6a76172a93247336b2b4581029037
mynz <rollingteapot@gmail.com>
2019-04-28 17:08:06 +0900

    add Image struct

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,20 @@ use std::fs::File;
 //use std::io::{BufReader, Read};
 use std::io::Read;
 
-fn load_bin(filename: String) {
+struct Image {
+    bin: Vec<u8>,
+}
+
+impl Image {
+    fn signature(&self) -> [u8; 4] {
+        let mut ret = [0u8; 4];
+        let b = &self.bin[0..4];
+        ret.copy_from_slice(b);
+        ret
+    }
+}
+
+fn load_image(filename: String) -> Image {
     let fp = File::open(filename).unwrap();
     let mut reader = std::io::BufReader::new(fp);
 
@@ -20,17 +33,21 @@ fn load_bin(filename: String) {
     let sig = std::str::from_utf8(&header[0..4]).unwrap();
     assert_eq!(sig, "NES\u{1a}");
 
-
     nread = reader.read_to_end(&mut bin).unwrap();
     println!("{:?}", nread);
 
-    for (i, b) in bin.into_iter().enumerate() {
+    for (i, b) in bin.iter().enumerate() {
         println!("b: {}P {:?}", i, b);
     }
+
+    Image { bin: bin }
 }
 
 fn main() {
     println!("Hello, world!");
 
-    load_bin("rom/sample1.nes".to_string());
+    let image = load_image("rom/sample1.nes".to_string());
+
+    println!("image size: {}", image.bin.len());
+    println!("image signature: {:?}", image.signature());
 }
