ea3875390481c3725f64d7808bb88afbfd452088
mynz <rollingteapot@gmail.com>
2019-04-28 18:36:27 +0900

    add get_header()

--- src/main.rs
+++ src/main.rs
@@ -11,45 +11,34 @@ struct Image {
 }
 
 impl Image {
-    fn signature(&self) -> [u8; 4] {
+    fn get_signature(&self) -> [u8; 4] {
         let mut ret = [0u8; 4];
         ret.copy_from_slice(&self.bin[0..4]);
         ret
+    }
+
+    fn get_header(&self) -> &[u8] {
+        &self.bin[0..16]
     }
 }
 
 fn load_image(filename: String) -> Image {
     let fp = File::open(filename).unwrap();
-    let mut reader = std::io::BufReader::new(fp);
-
     let mut bin: Vec<u8> = Vec::with_capacity(1024 * 1024);
-
-    let mut nread;
-    let mut header = [0u8; 16];
-    nread = reader.read(&mut header).unwrap();
-    println!("header: {}, {:?}", nread, header);
-
-    let sig = std::str::from_utf8(&header[0..4]).unwrap();
-    assert_eq!(sig, "NES\u{1a}");
-
-    nread = reader.read_to_end(&mut bin).unwrap();
-    println!("{:?}", nread);
-
-    for (i, b) in bin.iter().enumerate() {
-        println!("b: {}P {:?}", i, b);
-    }
-
+    let mut reader = std::io::BufReader::new(fp);
+    reader.read_to_end(&mut bin).unwrap();
     Image { bin: bin }
 }
 
 #[test]
 fn test_image() {
     assert!(true);
-
     let image = load_image("rom/sample1.nes".to_string());
-
-    let sig = image.signature();
+    let sig = image.get_signature();
     assert_eq!(sig, "NES\u{1a}".as_bytes());
+
+    let header = image.get_header();
+    assert_eq!(16, header.len())
 }
 
 fn main() {
@@ -58,5 +47,6 @@ fn main() {
     let image = load_image("rom/sample1.nes".to_string());
 
     println!("image size: {}", image.bin.len());
-    println!("image signature: {:?}", image.signature());
+    println!("image signature: {:#?}", image.get_signature());
+    println!("image header: {:?}", image.get_header());
 }
