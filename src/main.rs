110e2b9bb6097518db4d3278d797b49d04c2899b
mynz <rollingteapot@gmail.com>
2019-05-01 22:06:04 +0900

    try to write chr

--- src/main.rs
+++ src/main.rs
@@ -66,14 +66,54 @@ fn test_image() {
     assert_eq!(image.get_bytes_of_chr(), image.get_chr().len());
 }
  
-fn write_png() {
-    let buf = [0u8; 32 * 32 * 4];
+fn write_png(chr: &[u8]) {
 
-    image::save_buffer(&Path::new("tmp/image.png"), &buf, 32, 32, image::RGB(8));
+    let nblocks = chr.len() / 16;
+    let w = 8 * 64; // 512
+    let h = (nblocks / 64) * 8;
+
+    println!("write_png: {}, nblocks: {}, w,h: {}, {}", chr.len(), nblocks, w, h);
+
+    let nbuf: usize = w * h * 4;
+    let mut buf: Vec<u8> = vec![0u8; nbuf];
+
+    // 一ブロックは 16 byte 
+    for (idx_block, it) in chr.chunks(16).enumerate() {
+
+        let xb = idx_block % 64;
+        let yb = idx_block / 64;
+        let x_base = xb * 8;
+        let y_base = yb * 8;
+
+        //println!("it: {:?}, idx_block: {}, xb, yb: {}, {}, (x,y): {:?} ", it, idx_block, xb, yb, (x, y));
+
+        for iy in 0..8 {
+            for ix in 0..8 {
+
+                let y = iy + y_base;
+                let x = ix + x_base;
+                let dst_index = y * w * 4 + x * 4 + 0;
+                let line = it[iy];
+                let bit = (line >> ix) & 0x1;
+                let c: u8 = if bit != 0 {
+                    0xff
+                } else {
+                    0x00
+                };
+
+                buf[dst_index + 0] = c;
+                buf[dst_index + 1] = c;
+                buf[dst_index + 2] = c;
+                buf[dst_index + 3] = 0xff;
+            }
+        }
+    }
+
+    image::save_buffer(&Path::new("tmp/image.png"), &buf, w as u32, h as u32, image::RGBA(8));
 }
 
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
 
     let image = load_image("rom/sample1.nes".to_string());
 
@@ -91,5 +131,5 @@ fn main() {
     //println!("prg: {:?}", image.get_chr());
     //println!("chr: {:?}", image.get_chr());
 
-    write_png();
+    write_png(image.get_chr());
 }
