9bf3c66f769e82b62599db112bece67119fefd1f
mynz <rollingteapot@gmail.com>
2019-07-08 00:05:08 +0900

    impl cpu.rs

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,2 +1,63 @@
+#![allow(dead_code)]
 
+fn u8_to_i8(u: u8) -> i8 {
+    unsafe { std::mem::transmute::<u8, i8>(u) }
+}
 
+enum AddrMode {
+    Implied,
+    Immediate(u8),
+    ZeroPage(u8),
+    ZeroPageX(u8),
+    ZeroPageY(u8),
+    Absolute(u16),
+    AbsoluteX(u16),
+    AbsoluteY(u16),
+    Relative(u8),
+    Indirect(u16),
+    IndirectX(u8),
+    IndirectY(u8),
+}
+
+struct StatusRegister {
+    negative: bool,
+    overflow: bool,
+    reserved: bool,
+    brk: bool, // break
+    decimal: bool,
+    interrupt: bool,
+    zero: bool,
+    carry: bool,
+}
+
+impl Default for StatusRegister {
+    fn default() -> Self {
+        StatusRegister {
+            negative: false,
+            overflow: false,
+            reserved: true,
+            brk: true, // break
+            decimal: false,
+            interrupt: true,
+            zero: false,
+            carry: false,
+        }
+    }
+}
+#[derive(Default)]
+struct Register {
+    a: u8, // accumelater
+    x: u8, // index register
+    y: u8, // index register
+    s: u8, // stack pointer
+    p: StatusRegister,
+    pc: u16,
+}
+
+impl Register {}
+
+#[test]
+fn test_cpu() {
+    assert!(true);
+    assert_eq!(1, 1);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod color_palette;
-pub mod rom;
 pub mod cpu;
+pub mod rom;
