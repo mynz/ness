1af9e07acca3cf548bf6e737057e646cffb449ba
mynz <rollingteapot@gmail.com>
2019-07-28 23:14:56 +0900

    save_in_png

--- src/ppu/frame_buffer.rs
+++ src/ppu/frame_buffer.rs
@@ -1,4 +1,7 @@
-struct Color(u8, u8, u8);
+extern crate image;
+use std::path::Path;
+
+struct RGB(u8, u8, u8);
 
 struct FrameBuffer {
     w: u32,
@@ -16,11 +19,15 @@ impl FrameBuffer {
         }
     }
 
-    fn set_pixel(&mut self, x: u32, y: u32, c: &Color) {
+    fn set_pixel(&mut self, x: u32, y: u32, c: &RGB) {
         let p = 3 * (self.w * y + x) as usize;
         self.buf[p + 0] = c.0;
         self.buf[p + 1] = c.1;
         self.buf[p + 2] = c.2;
+    }
+
+    fn save_in_png<P: AsRef<Path>>(&self, path: P) {
+        image::save_buffer(path, &self.buf, self.w, self.h, image::RGB(8)).unwrap();
     }
 }
 
@@ -36,11 +43,16 @@ mod tests {
         assert_ne!(fb.buf.len(), 0);
     }
 
+    #[test]
     fn test_save_in_png() {
-        let mut fb = FrameBuffer::new(320, 240);
-        fb.set_pixel(0, 0, &Color(0xff, 0, 0));
-        fb.set_pixel(1, 0, &Color(0xff, 0, 0));
-        fb.set_pixel(2, 0, &Color(0xff, 0, 0));
+        let mut fb = FrameBuffer::new(32, 24);
+        fb.set_pixel(0, 0, &RGB(0xff, 0, 0));
+        fb.set_pixel(1, 0, &RGB(0xff, 0, 0));
+        fb.set_pixel(2, 0, &RGB(0xff, 0, 0));
+        fb.set_pixel(0, 1, &RGB(0, 0xff, 0));
+        fb.set_pixel(1, 1, &RGB(0, 0, 0xff));
+        fb.set_pixel(2, 1, &RGB(0, 0xff, 0xff));
+        fb.save_in_png("test_image00.png");
     }
 
 }
