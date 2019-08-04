78965e47a9a49914ec8244a4137458bc8ea95f33
mynz <rollingteapot@gmail.com>
2019-08-05 02:37:43 +0900

    new src/tests.rs

---
+++ src/tests.rs
@@ -0,0 +1,17 @@
+#![cfg(test)]
+
+#[test]
+fn test_render_bg() {
+    use crate::cpu::*;
+    use crate::rom::*;
+
+    let mut exe = Executer::new();
+
+    let rom = Rom::load_image("static/sample1/sample1.nes".to_string());
+    exe.set_rom(rom);
+    exe.hard_reset();
+
+    for _ in 0..64 {
+        exe.execute();
+    }
+}
