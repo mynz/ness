ea706095f903359bfc36b4956ff2f04f7621937a
mynz <rollingteapot@gmail.com>
2019-09-15 01:05:40 +0900

    disable assert

--- src/ppu.rs
+++ src/ppu.rs
@@ -253,7 +253,7 @@ impl PpuUnit {
         match addr {
             0x2000 => {
                 self.reg.ctrl = data;
-                assert!(self.reg.ctrl & 0x3 == 0, "not supported yet: {:b}", data);
+                //assert!(self.reg.ctrl & 0x3 == 0, "not supported yet: {:b}", data);
             }
             0x2001 => {
                 self.reg.mask = data;
